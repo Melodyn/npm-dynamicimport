3d2834ec3ff4baf5bdf0086cfd4af19f08d423c8
melodyn <samelodyn@gmail.com>
2019-12-01 17:50:07 +0300

    added typings, tests

--- __tests__/lib/index.test.js
+++ __tests__/lib/index.test.js
@@ -1,5 +1,21 @@
+import * as path from 'path';
+import autoloader from '../../lib';
+
+const correctProjectRoot = path.resolve(__dirname, '..', 'fixtures', 'backend');
+const incorrectProjectRoot = path.resolve(__dirname, '..', '..');
+
 describe('Positive cases', () => {
-  test('First test', () => {
-    expect(2 * 2).toEqual(4);
+  test('Just test', async () => {
+    await expect(autoloader(correctProjectRoot)).resolves.toEqual('ok');
+  });
+});
+
+describe('Negative cases', () => {
+  test('Incorrect arguments', async () => {
+    await expect(autoloader(undefined, undefined)).rejects.toThrow('Missing project root');
+    await expect(autoloader('/', [])).rejects.toThrow('Not found package.json in /package.json');
+    await expect(autoloader(incorrectProjectRoot)).rejects.toThrow(
+      "Missing field 'autoload' in /usr/src/app/package.json",
+    );
   });
 });
