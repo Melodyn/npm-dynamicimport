ecaaab4372a5f940336e0997281d91f3a99d3b07
melodyn <samelodyn@gmail.com>
2019-12-02 06:52:32 +0300

    added test

--- __tests__/lib/index.test.js
+++ __tests__/lib/index.test.js
@@ -14,6 +14,7 @@ describe('Negative cases', () => {
       cases: { invalidPackageJson },
     } = fixtures;
     await expect(autoloader(undefined, undefined)).rejects.toThrow('Missing project root');
+    await expect(autoloader('/aaaa')).rejects.toThrow('Path /aaaa not exists');
     await expect(autoloader('/', [])).rejects.toThrow('Not found package.json in /package.json');
     await expect(autoloader(invalidPackageJson)).rejects.toThrow(
       "Missing field 'autoload' in /usr/src/app/package.json",
