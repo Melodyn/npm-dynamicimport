33039e991877365a938e00053aae55eb37f6a704
melodyn <samelodyn@gmail.com>
2019-12-03 23:02:46 +0300

    load modules functional

--- __tests__/fixtures/index.js
+++ __tests__/fixtures/index.js
@@ -5,10 +5,10 @@ const projectRoot = path.resolve(__dirname, 'backend');
 export default {
   projectRoot,
   cases: {
-    validUserConfig_index: ['backend', 'modules', '*', 'index.js|ts'],
-    validUserConfig_module: ['backend', 'modules', '*', 'module.js|ts'],
+    validUserConfig_index: ['modules', '*', 'index.js|ts'],
+    validUserConfig_module: ['modules', '*', 'module.js|ts'],
     validPackageJson: projectRoot,
-    invalidModulePath: ['backend', 'module.js|ts'],
+    invalidModulePath: ['module.js|ts'],
     invalidPackageJson: path.resolve(__dirname, '..', '..'),
   },
   expected: {},
